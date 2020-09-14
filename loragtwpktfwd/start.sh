5eca0cf15086262f6076d0f96951bc2dcf5b58fe
PastaGringo <vincentdelbeau@macbook-pro-de-vincent.home>
2020-09-14 02:25:08 +0200

    first commit

--- loragtwpktfwd/start.sh
+++ loragtwpktfwd/start.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# Reset RAK2245 PIN
-SX1301_RESET_BCM_PIN=17
-echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction
-echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
-sleep 0.1
-echo "1"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
-sleep 0.1
-echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
-sleep 0.1
-echo "$SX1301_RESET_BCM_PIN" > /sys/class/gpio/unexport
-sleep 0.2
-cd /build/packet_forwarder/lora_pkt_fwd
-./lora_pkt_fwd
+cd /build/sx1302_hal/bin
+
+# Reset RAK2287 PIN
+./reset_lgw.sh
+
+# Starting LoRaWAN Packet Forwarder
+./lora_pkt_fwd -c global_conf.json.sx1250
